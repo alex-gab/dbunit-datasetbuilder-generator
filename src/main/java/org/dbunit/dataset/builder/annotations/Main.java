5485295173c3d0ad7e942787a40fc9a212fac0e2
alex-gab <dragoi.alex@gmail.com>
2016-10-14 07:48:13 +0300

    [#1]: Initial commit

--- src/main/java/org/dbunit/dataset/builder/annotations/Main.java
+++ src/main/java/org/dbunit/dataset/builder/annotations/Main.java
@@ -1,12 +1,32 @@
 package org.dbunit.dataset.builder.annotations;
 
-import org.dbunit.dataset.DataSetException;
-import org.dbunit.dataset.builder.ColumnSpec;
-import org.dbunit.dataset.builder.DataSetBuilder;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
+
+import java.io.*;
 
 public final class Main {
-    public static void main(String[] args) throws DataSetException {
-        DataSetBuilder builder = new DataSetBuilder();
-        ColumnSpec<Integer> age = ColumnSpec.newColumn("AGE");
+    public static void main(String[] args) throws IOException, JSQLParserException {
+        String[] sqlStatements = readSql("schema.sql");
+
+        for (String sqlStatement : sqlStatements) {
+            CreateTable statement = (CreateTable) CCJSqlParserUtil.parse(new StringReader(sqlStatement));
+            System.out.println(statement);
+        }
+    }
+
+
+    public static String[] readSql(String schema) throws IOException {
+        final InputStream schemaStream = ClassLoader.getSystemResourceAsStream(schema);
+        BufferedReader br = new BufferedReader(new InputStreamReader(schemaStream));
+        String mysql = "";
+        String line;
+        while ((line = br.readLine()) != null) {
+            mysql = mysql + line;
+        }
+        br.close();
+        mysql = mysql.replaceAll("`", "");
+        return mysql.split(";");
     }
 }
